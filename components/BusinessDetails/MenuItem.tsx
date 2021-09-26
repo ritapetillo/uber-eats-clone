381411da05d490955d5c3d1f5b7d047ee6a55380
ritapetillo <rita.petillo@me.com>
2021-09-26 03:47:18 +0200

    completed variations

--- components/BusinessDetails/MenuItem.tsx
+++ components/BusinessDetails/MenuItem.tsx
@@ -1,5 +1,6 @@
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 interface MenuItemProps {
   item: {
@@ -12,17 +13,25 @@ interface MenuItemProps {
 }
 
 const MenuItem = ({ item }: MenuItemProps) => {
+  const navigation = useNavigation();
   return (
-    <View style={tw`flex-row p-4`} key={item.id}>
-      <View style={tw`flex-grow`}>
-        <Text style={tw`text-base font-medium`}>{item.name}</Text>
-        <Text style={tw`text-base font-light leading-8`}>$ {item.price}</Text>
-        <Text style={tw`text-gray-500`}>{item.description}</Text>
+    <TouchableOpacity
+      onPress={() => navigation.navigate("Item" as never, { item } as never)}
+    >
+      <View style={tw`flex-row p-4`} key={item.id}>
+        <View style={tw`flex-grow`}>
+          <Text style={tw`text-base font-medium`}>{item.name}</Text>
+          <Text style={tw`text-base font-light leading-8`}>$ {item.price}</Text>
+          <Text style={tw`text-gray-500`}>{item.description}</Text>
+        </View>
+        <View>
+          <Image
+            source={{ uri: item.image_url }}
+            style={tw`h-24 w-24 rounded`}
+          />
+        </View>
       </View>
-      <View>
-        <Image source={{ uri: item.image_url }} style={tw`h-24 w-24 rounded`} />
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
